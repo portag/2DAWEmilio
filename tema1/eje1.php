dd158304c4f800785d996727e464f3efa6e22d0d
portag <emiliodavid.zrgn@gmail.com>
2022-09-23 18:12:17 +0200

    ejemplo

--- tema1/eje1.php
+++ tema1/eje1.php
@@ -5,7 +5,7 @@
       <div class="col-md-8 themed-grid-col">
 		<div class="flex-shrink-0 p-3 bg-white">
 			<?php
-				echo "Aquí vendría el código del ejercicio1 que estoy haciendo";
+				echo "Aquí vendría el código del ejercicio1 quee estoy haciendo";
 			?>
 		</div>
 	  </div>

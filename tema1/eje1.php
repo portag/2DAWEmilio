d5ed7ea24071cf2025468df18464189e7958cfb9
portag <emiliodavid.zrgn@gmail.com>
2022-09-23 18:22:31 +0200

    otro commit

--- tema1/eje1.php
+++ tema1/eje1.php
@@ -5,7 +5,21 @@
       <div class="col-md-8 themed-grid-col">
 		<div class="flex-shrink-0 p-3 bg-white">
 			<?php
-				echo "Aquí vendría el código del ejercicio1 quee estoy haciendo";
+				echo "Primer ejemplo";
+
+				echo "<table>";
+				$contador = 1;
+				for($i=0; $i<5; $i++){
+					echo "<tr>";
+
+					for($j=0; $j<3; $j++){
+						echo "<td>" . $contador . "</td>";
+						$contador++;
+						
+					}
+					echo "</tr>";
+				}
+				echo "</table>"
 			?>
 		</div>
 	  </div>
---
+++ tema1/eje2.php
@@ -0,0 +1,20 @@
+<?php
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/cabecera.php");
+?>
+
+      <div class="col-md-8 themed-grid-col">
+		<div class="flex-shrink-0 p-3 bg-white">
+			<?php
+				$numero = 6;
+
+                for($i = 1; $i <=10; $i++){
+                    echo $numero . "*" . $i . " = " . $numero*$i;
+                }
+			?>
+		</div>
+	  </div>
+    </div>
+
+<?php
+    include_once("../pie.php");
+?>
